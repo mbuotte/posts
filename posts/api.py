262ef785e4fb087ccc7062119e690a608bd08c4a
Maureen Buotte <mbuotte@eagleaccess.com>
2015-03-01 14:43:08 +0000

    Add require

--- posts/api.py
+++ posts/api.py
@@ -77,6 +77,7 @@ def post_delete(id):
   
 @app.route("/api/posts", methods=["POST"])
 @decorators.accept("application/json")
+@decorators.require("application/json")
 def posts_post():
     """ Add a new post """
     data = request.json
